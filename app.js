a4da48bd2feb02597af6ec724c34901c837c8acd
Rohit00009 <132641794+Rohit00009@users.noreply.github.com>
2024-01-13 18:52:05 +0530

    panel styling

--- app.js
+++ app.js
@@ -34,6 +34,15 @@ function randomColors() {
     hexText.innerText = randomColor;
     //check for contrast
     checkTextContrast(randomColor, hexText);
+
+    //initialize oloroize sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input"); //now w can access all sliders
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
   });
 }
 
@@ -44,6 +53,26 @@ function checkTextContrast(color, text) {
   } else {
     text.style.color = "white";
   }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  //scale saturation
+  const noSat = color.set("hsl.s", 0); //we take color.set functionality from chromajs
+  const fullSat = color.set("hsl.s", 1); //hsl.s for saturation
+  const scaleSat = chroma.scale([noSat, color, fullSat]);
+
+  //scale brightness
+  const midbright = color.set("hsl.l", 0.5); //hsl.l for brightness ---> we give only mid cu satrt and end we know are white and black.
+  const scaleBright = chroma.scale(["black", midbright, "white"]);
+
+  //update input colors
+  saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(
+    0
+  )} , ${scaleSat(1)})`;
+
+  brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(
+    0
+  )}, ${scaleBright(0.5)}, ${scaleBright(1)})`;
 }
 
 randomColors();
