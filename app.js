7b43da324c160dc6b7380f7b9bbf5de9c318b8bd
Rohit00009 <132641794+Rohit00009@users.noreply.github.com>
2024-01-15 23:00:38 +0530

    functionaliy of panel

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll(".color h2");
 const popup = document.querySelector(".copy-container");
+const adjustButton = document.querySelectorAll(".adjust");
+const closeAdjustments = document.querySelectorAll(".close-adjustment");
+const sliderContainers = document.querySelectorAll(".sliders");
 let initialColors; //we created variable!
 
 //eventlistners
@@ -24,6 +27,16 @@ popup.addEventListener("transitionend", () => {
   const popupBox = popup.children[0];
   popup.classList.remove("active");
   popupBox.classList.remove("active");
+});
+adjustButton.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    openAdjustmentPanel(index);
+  });
+});
+closeAdjustments.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    closeAdjustmentPanel(index);
+  });
 });
 
 //FUNCTIONS
@@ -71,6 +84,9 @@ function randomColors() {
   });
   //reset inputs
   resetInputs();
+
+  //check for button contrast
+  
 }
 
 function checkTextContrast(color, text) {
@@ -174,6 +190,13 @@ function copyToClipboard(hex) {
   const popupBox = popup.children[0];
   popup.classList.add("active");
   popupBox.classList.add("active");
+}
+
+function openAdjustmentPanel(index) {
+  sliderContainers[index].classList.toggle("active");
+}
+function closeAdjustmentPanel(index) {
+  sliderContainers[index].classList.remove("active");
 }
 
 randomColors();
